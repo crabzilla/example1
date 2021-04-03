8e37782e896bb3fec770e1f41c55cdaa714fad64
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-04-03 16:27:54 -0300

    missing files

---
+++ apps/queries-handler/src/main/kotlin/com/example1/customers/CustomersQueryDao.kt
@@ -0,0 +1,23 @@
+package com.example1.customers
+
+import com.example1.infra.handle
+import com.example1.jooq.tables.CustomerSummary.CUSTOMER_SUMMARY
+import com.example1.jooq.tables.pojos.CustomerSummary
+import io.vertx.core.Future
+import io.vertx.core.Promise
+import io.zero88.jooqx.DSLAdapter
+import io.zero88.jooqx.ReactiveJooqx
+import javax.inject.Singleton
+
+@Singleton
+class CustomersQueryDao(private val jooqx: ReactiveJooqx) {
+
+    fun all(): Future<List<CustomerSummary>> {
+        val promise = Promise.promise<List<CustomerSummary>>()
+        val sql = jooqx.dsl().select().from(CUSTOMER_SUMMARY).orderBy(CUSTOMER_SUMMARY.ID)
+        val dslAdapter = DSLAdapter.fetchMany(CUSTOMER_SUMMARY, CustomerSummary::class.java)
+        jooqx.execute(sql, dslAdapter) { it.handle(promise) }
+        return promise.future()
+    }
+
+}
