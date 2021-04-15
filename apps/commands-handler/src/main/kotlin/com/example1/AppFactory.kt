c55a4ac3f2334b73410702f65de0196a073d5e49
rodolfodpk <rodolfo_dpk@yahoo.com>
2021-04-15 13:35:18 -0300

    in memory snapshot repository

--- apps/commands-handler/src/main/kotlin/com/example1/AppFactory.kt
+++ apps/commands-handler/src/main/kotlin/com/example1/AppFactory.kt
@@ -7,9 +7,11 @@ import io.github.crabzilla.example1.CustomerEvent
 import io.github.crabzilla.example1.customerConfig
 import io.github.crabzilla.pgc.CommandControllerFactory
 import io.github.crabzilla.stack.CommandController
+import io.github.crabzilla.stack.InMemorySnapshotRepo
 import io.micronaut.context.annotation.Bean
 import io.micronaut.context.annotation.Context
 import io.micronaut.context.annotation.Factory
+import io.vertx.core.Vertx
 import io.vertx.pgclient.PgPool
 import javax.inject.Named
 
@@ -20,9 +22,10 @@ private class AppFactory {
 
     @Bean
     @Context
-    fun customerCommandController(@Named("writeDb") writeDb: PgPool):
+    fun customerCommandController(vertx: Vertx, @Named("writeDb") writeDb: PgPool):
             CommandController<Customer, CustomerCommand, CustomerEvent> {
-        return CommandControllerFactory.createPublishingTo(boundedContextName.name, customerConfig, writeDb)
+        val snapshotRepo = InMemorySnapshotRepo(vertx.sharedData(), customerConfig)
+        return CommandControllerFactory.createPublishingTo(boundedContextName.name, customerConfig, writeDb, snapshotRepo)
     }
 
 }
